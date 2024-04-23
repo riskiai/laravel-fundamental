6a82c726b43862e8b8a8bb986a03c7d57558cc3e
riskiai <riskiahmadiilham@gmail.com>
2024-04-23 14:44:10 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/list-menu', function () {
+    return "Ini adalah halaman yang menampilkan semua menu yang ada di Cafe Amandemy";
+});
+
+
+Route::get('/list-menu/get-all-data' , function() {
+    $data = [
+        ['makanan' => 'Soto Mie', 'harga' => 8000],
+        ['makanan' => 'Bakso Malang', 'harga' => 13000],
+        ['makanan' => 'Ketoprak', 'harga' => 15000]
+    ];
+
+    return response()->json(['info' => 'data berhasil didapatkan', 'data' => $data]);
+});
+
+Route::get('/list-menu/{makanan}/{harga}', function ($makanan, $harga) {
+    return "Anda memilih makanan $makanan dengan harga $harga ";
+});
+
